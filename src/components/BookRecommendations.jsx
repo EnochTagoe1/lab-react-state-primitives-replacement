bc2f391df217d236feedacba603622cd59c8f056
enochtagoe <enochtagoe@pursuit.org>
2023-12-11 00:32:28 -0500

    push updates

--- src/components/BookRecommendations.jsx
+++ src/components/BookRecommendations.jsx
@@ -15,8 +15,8 @@ const BookRecommendations = () => {
   // TODO: Implement state for recommendations
   // TODO: Implement the handleGenreSelect function
 const handleGenreSelect = (genre) => {
-  const bookChoice = bookData[genre];
-  setNewRecommendations(bookSelection);
+  const bookChoice = books[genre];
+  setNewRecommendations(bookChoice);
   setNewGenre(genre);
 }
 
@@ -25,14 +25,29 @@ const handleGenreSelect = (genre) => {
       <h2>Book Recommendation Engine</h2>
       <div className="genre-buttons">
         {/* TODO: Map over genres and create buttons */}
-        {Object.keys(books).map(genre => {return <button>{genre}</button>})}
+        {Object.keys(books).map(genre => {return <button
+          key = {genre}
+          className = {newGenre === genre && "active"}
+          onClick = {() => {handleGenreSelect(genre)}} >
+            {genre}
+        </button>})}
       </div>
       <div>
         <h3>Recommendations:</h3>
         {/* TODO: Display recommendations based on selected genre */}
+        {newRecommendations.length > 0 &&  
+          <ul>
+            {newRecommendations.map((bookTitles, index) => (
+             <li key = {index}>
+              {bookTitles}
+             </li> 
+            ))}
+          </ul>
+         (<p>Choose A Genre</p>
+        )}
       </div>
     </div>
-  );
+  )
 };
 
 export default BookRecommendations;
